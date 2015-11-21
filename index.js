63bb82481a55e6906a18f83f856465da19277225
Farhad Ghayour <me@farhadg.com>
2015-11-21 15:03:26 -0500

    Add: rename, src, dest and link transform streams added to Au's prototype

--- index.js
+++ index.js
@@ -5,17 +5,7 @@
  */
 
 var TaskManager = require('undertaker');
-var through = require('through2');
-var request = require('superagent');
-var es = require('event-stream');
-var cheerio = require('cheerio');
-var mkdirp = require('mkdirp');
-var isArray = Array.isArray;
 var util = require('util');
-var path = require('path');
-var fs = require('fs');
-var rs = fs.ReadStream;
-var ws = fs.WriteStream;
 
 /**
  * Initialize
@@ -23,6 +13,7 @@ var ws = fs.WriteStream;
 
 function Au() {
   TaskManager.call(this);
+  this.streamStore = {};
 }
 
 /**
@@ -32,56 +23,19 @@ function Au() {
 util.inherits(Au, TaskManager);
 
 /**
- * Private helper function for creating a readable stream
- * and configuring a default file path.
- *
- * @param url
- * @returns {Request}
+ * Sets the accessible methods on the prototype
  */
 
-function createReadableStream(url) {
-  var stream = request.get(url);
-  stream.filePath = stream.url
-    .replace(/http|https|:\/\//g, '')
-    .replace(/\/|\./g, '-');
-  return stream;
+Au.prototype = {
+  rename: require('./lib/rename'),
+  link: require('./lib/link'),
+  dest: require('./lib/dest'),
+  src: require('./lib/src'),
+  Au: Au
 };
 
 /**
- * Configures and sets the source
- */
-
-Au.prototype.src = function src(urls) {
-  urls = isArray(urls) ? urls : [urls];
-  var stream = through.obj();
-  urls.forEach(function(url) {
-    stream.push(createReadableStream(url));
-  });
-  return stream;
-};
-
-/**
- * Configures and sets the destination
- */
-
-Au.prototype.dest = function dest(output) {
-  function transform(stream, enc, cb) {
-    mkdirp(path.join(__dirname, output));
-    var filePath = path.join(__dirname, output, stream.filePath);
-    stream.pipe(ws(filePath));
-    cb(null, stream);
-  }
-  return through.obj(transform);
-};
-
-/**
- * Expose the constructor on the prototype
- */
-
-Au.prototype.Au = Au;
-
-/**
- * Expose 'Au'
+ * Expose an instance of Au
  */
 
 module.exports = new Au();
